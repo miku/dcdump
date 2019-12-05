f70d0f78c7427822b00d0a487c6b43d070b08bb9
Martin Czygan <martin.czygan@gmail.com>
2019-12-05 23:51:29 +0100

    simplify example script

--- dcdump.sh
+++ dcdump.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
-
-# Example mini script to harvest datacite in three partial runs to mitigate API
-# errors due to huge result sets
-# (https://gist.github.com/miku/176edd1222fc42ae3b23234bc9d3cd87).
+#
+# Example script to harvest datacite. Since some results sets get large
+# (see: https://gist.github.com/miku/176edd1222fc42ae3b23234bc9d3cd87) using
+# minute intervals for all the data is a more expensive, but a bit more robust
+# approach.
 #
 # Note, that manual intervention might still be required, because of an
 # unexpected, non-recoverable HTTP 500 or HTTP 403.
+#
+# $ dcdump.sh [DIR]
 
 set -eu -o pipefail
 
-HARVEST_DIR=${1:-$(mktemp -d -t dcdump-XXXXXXXXXX)}
-echo >&2 "harvest dir: $HARVEST_DIR"
+HARVEST_DIR=${1:-$(mktemp -d -t dcdump-$(date +"%Y-%m-%d")-XXXXXXXXXX)}
+PATH="$PATH:$(pwd)" dcdump -i e -d "$HARVEST_DIR" -p "dcdump-"
 
-function finish() {
-	echo >&2 "harvest dir: $HARVEST_DIR"
-}
-trap finish EXIT
-
-PATH="$PATH:$(pwd)" dcdump -s 2018-01-01 -e '2019-07-31 23:59:59' -i daily -d tmp -p 'part-01-'
-PATH="$PATH:$(pwd)" dcdump -s 2019-08-01 -i e -d tmp -p 'part-02-'
-
-echo >&2 "done: $HARVEST_DIR"
