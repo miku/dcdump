c05451c247e79d1adffc02fba4d7fa8080249a45
Martin Czygan <martin.czygan@gmail.com>
2020-03-12 14:01:14 +0100

    update docs

--- dcdump.sh
+++ dcdump.sh
@@ -9,9 +9,17 @@
 # unexpected, non-recoverable HTTP 500 or HTTP 403.
 #
 # $ dcdump.sh [DIR]
+#
+# If DIR is not given, we create a default `dcdump-YYYY-MM-DD` in the current
+# directory.
 
 set -eu -o pipefail
 
-HARVEST_DIR=${1:-$(mktemp -d -t dcdump-$(date +"%Y-%m-%d")-XXXXXXXXXX)}
-PATH="$PATH:$(pwd)" dcdump -i e -d "$HARVEST_DIR" -p "dcdump-"
+HARVEST_DIR=${1:-dcdump-$(date +"%Y-%m-%d")}
+if [ ! -d "$HARVEST_DIR" ]; then
+	mkdir -p "$HARVEST_DIR"
+fi
 
+# Harvest with one slice per minute (-e), into harvest dir (-d) with a common
+# prefix (-p).
+PATH="$PATH:$(pwd)" dcdump -i e -d "$HARVEST_DIR" -p "dcdump-"
